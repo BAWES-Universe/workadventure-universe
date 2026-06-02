4560901982def8c5d7c2361fdd6b9d465a4cfc92
Khalid Al-Mutawa <khalid@bawes.net>
2026-06-03 00:09:29 +0300

    feat: add Sentry SDK to maps, uploader, bots, discord-bot

--- uploader/server.ts
+++ uploader/server.ts
@@ -1,4 +1,29 @@
+import * as Sentry from "@sentry/node";
 import App from "./src/App";
+import {
+    SENTRY_DSN,
+    SENTRY_RELEASE,
+    SENTRY_ENVIRONMENT,
+    SENTRY_TRACES_SAMPLE_RATE,
+} from "./src/Enum/EnvironmentVariable";
+
+// Sentry integration
+if (SENTRY_DSN != undefined) {
+    try {
+        const sentryOptions: Sentry.NodeOptions = {
+            dsn: SENTRY_DSN,
+            release: SENTRY_RELEASE,
+            environment: SENTRY_ENVIRONMENT,
+            tracesSampleRate: SENTRY_TRACES_SAMPLE_RATE,
+            attachStacktrace: true,
+        };
+
+        Sentry.init(sentryOptions);
+        console.info("Sentry initialized");
+    } catch (e) {
+        console.error("Error while initializing Sentry", e);
+    }
+}
 
 App.listen(8080, () => {
     console.log(`WorkAdventure uploader starting on port 8080!`);
