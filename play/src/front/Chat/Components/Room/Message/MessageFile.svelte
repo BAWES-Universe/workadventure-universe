83be67b80e090dabfdef15e0b494692345530c17
Khalid Al-Mutawa <khalid@bawes.net>
2026-07-13 03:42:17 +0300

    fix: replace mailbox icon with generic document icon for file attachments

--- play/src/front/Chat/Components/Room/Message/MessageFile.svelte
+++ play/src/front/Chat/Components/Room/Message/MessageFile.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import type { Readable } from "svelte/store";
     import type { ChatMessageContent } from "../../../Connection/ChatConnection";
-    import { IconInbox } from "@wa-icons";
+    import { IconFile } from "@wa-icons";
 
     export let content: Readable<ChatMessageContent>;
 </script>
@@ -14,7 +14,7 @@
     class="flex items-center text-white hover:bg-white/10 px-2 py-2 rounded-md hover:no-underline hover:text-white font-bold cursor-pointer"
 >
     <div class="flex items-center justify-center p-2 bg-white/10 rounded-full">
-        <IconInbox font-size="20" />
+        <IconFile font-size="20" />
     </div>
     <div class="px-4 truncate">
         {$content.body}
